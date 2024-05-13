7fe3e54f9f7dacf3739cfbd800dc1455bc0039d0
enayetaitech <mdrehman@365aitech.com>
2024-05-13 15:24:29 +0600

    3

--- app.js
+++ app.js
@@ -1,8 +1,61 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ObjectID } = require('mongodb');
+const cron = require('node-cron');
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+const uri = 'mongodb+srv://balpreet:ct8bCW7LDccrGAmQ@cluster0.2pwq0w2.mongodb.net/enayetTest'
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tls: true,  
+  });
+  let db;
+
+async function connect() {
+    try {
+        await client.connect();  // Attempt to connect
+        db = client.db(); // Assigns the database handle
+    } catch (error) {
+        console.error("Connection to MongoDB failed:", error);
+        throw error; // Rethrow or handle error appropriately
+    }
+    return db;
+}
+
+async function getDb() {
+    if (!db) {
+        await connect();
+    }
+    return db;
+}
+
+async function getCollections() {
+    const db = await getDb();
+    return {
+    
+        videoCollection: db.collection("FinalVideo")
+    };
+}
+
+async function addToCollection() {
+  try {
+      const { videoCollection } = await getCollections();
+      await videoCollection.insertOne({ message: "Hello" });
+      console.log("Added 'Hello' to videoCollection");
+  } catch (error) {
+      console.error("Failed to add 'Hello' to videoCollection:", error);
+  }
+}
+
+cron.schedule('*/10 * * * * *', async () => {
+  console.log('Running a task every 10 seconds');
+  await addToCollection();
 });
 
 const PORT = process.env.PORT || 3000;
